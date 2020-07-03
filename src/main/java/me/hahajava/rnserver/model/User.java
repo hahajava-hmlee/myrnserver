edcd7b4289a34fcc74368822e008042a13aca818
hahava <hahava@nts-corp.com>
2020-07-03 20:19:19 +0900

    Change model relationship

--- src/main/java/me/hahajava/rnserver/model/User.java
+++ src/main/java/me/hahajava/rnserver/model/User.java
@@ -1,14 +1,10 @@
 package me.hahajava.rnserver.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-
-import static com.fasterxml.jackson.annotation.JsonProperty.Access.READ_ONLY;
 
 @Entity
 @Getter
@@ -17,22 +13,21 @@ public class User {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@JsonProperty(access = READ_ONLY)
 	@Column(name = "no")
 	private Long userNo;
 
 	@NotNull
-	@Column(nullable = false, unique = true)
+	@Column(nullable = false,
+			unique = true,
+			name = "userId")
 	private String id;
 
 	@NotNull
-	@Column(nullable = false)
+	@Column(nullable = false, name = "USER_PW")
 	private String userPw;
 
 	@NotNull
+	@Column(name = "TOKEN")
 	private String token;
 
-	@JsonIgnore
-	@OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
-	private Profile profile;
 }
